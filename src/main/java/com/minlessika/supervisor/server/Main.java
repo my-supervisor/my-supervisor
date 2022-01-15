c25563ed4e1c32cf6e96d759c4b3846d449f84aa
Olivier B. OURA <baudoliver7@gmail.com>
2022-01-15 23:33:58 +0000

    feat: enable liquibase
    We replaced the old database management by liquibase.
    
    Fix: #7

--- src/main/java/com/minlessika/supervisor/server/Main.java
+++ src/main/java/com/minlessika/supervisor/server/Main.java
@@ -1,51 +1,61 @@
 package com.minlessika.supervisor.server;
 
-import java.util.Arrays;
-import javax.sql.DataSource;
-
+import com.minlessika.core.takes.TkApp;
+import com.minlessika.sdk.app.info.AppInfoImpl;
+import com.minlessika.sdk.datasource.Base;
+import com.minlessika.sdk.pgsql.PgBase;
+import com.minlessika.sdk.websockets.WebSocketNettosphere;
+import com.minlessika.sdk.websockets.WebSocketServer;
+import com.minlessika.db.BasicDatabase;
+import com.minlessika.db.DatabaseLiquibaseUpdate;
 import com.minlessika.membership.domain.Membership;
-import com.minlessika.sdk.pgsql.statement.PgCreateDatabase;
-import com.minlessika.sdk.pgsql.statement.PgDatabaseExistsStatement;
+import com.minlessika.utils.ConsoleArgs;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.Arrays;
+import java.util.Map;
+import javax.sql.DataSource;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
 import org.takes.http.Exit;
 import org.takes.http.FtCli;
-import com.minlessika.core.takes.TkApp;
-import com.minlessika.sdk.app.info.AppInfo;
-import com.minlessika.sdk.app.info.AppInfoImpl;
-import com.minlessika.sdk.datasource.Base;
-import com.minlessika.sdk.datasource.BaseCredentials;
-import com.minlessika.sdk.datasource.DataSources;
-import com.minlessika.sdk.pgsql.PgBase;
-import com.minlessika.sdk.pgsql.PgBaseCredentials;
-import com.minlessika.sdk.pgsql.PgHikariDataSourceFactory;
-import com.minlessika.sdk.websockets.WebSocketNettosphere;
-import com.minlessika.sdk.websockets.WebSocketServer;
-import com.minlessika.supervisor.domain.Supervisor;
 
 public final class Main {
 	
     public static void main(final String... args) throws Exception {
-    	    	    
-    	AppInfo appInfo = new AppInfoImpl(Membership.NAME, args);
-    	BaseCredentials credentials = new PgBaseCredentials(args);
-		DataSources dataSourceFactory = new PgHikariDataSourceFactory(credentials);
-		final DataSource sysSource = dataSourceFactory.create("postgres");
-		final Base baseSys = new PgBase(sysSource);
-
-		final boolean databaseExists = new PgDatabaseExistsStatement(baseSys, credentials.basename()).exists();
-		if(!databaseExists)
-			new PgCreateDatabase(baseSys, credentials.basename()).execute();
-		DataSource dataSource = dataSourceFactory.create();
-		
-		WebSocketServer wsServer = new WebSocketNettosphere(
-										Arrays.asList(ActivityRealtime.class), 
-										args
-								   );
-		wsServer.start();
-		
-		Base base = new PgBase(dataSource, appInfo, wsServer);
-    	
+		final Map<String, String> map = new ConsoleArgs("--", args).asMap();
+		final HikariConfig config = new HikariConfig();
+		config.setDriverClassName("org.postgresql.Driver");
+		config.setJdbcUrl(
+			String.format(
+				"jdbc:postgresql://%s:%s/%s",
+				map.get("db-host"),
+				map.get("db-port"),
+				map.get("database")
+			)
+		);
+		config.setUsername(map.get("db-user"));
+		config.setPassword(map.get("db-password"));
+		int mps = 25;
+		if (map.containsKey("db-max-pool-size")) {
+			mps = Integer.parseInt(map.get("db-max-pool-size"));
+		}
+		config.setMaximumPoolSize(mps);
+		final DataSource source = new HikariDataSource(config);
+		new DatabaseLiquibaseUpdate(
+			new BasicDatabase(source),
+			"liquibase/db.changelog-master.xml"
+		).start();
+		final WebSocketServer wss = new WebSocketNettosphere(
+			Arrays.asList(ActivityRealtime.class),
+			args
+	    );
+		wss.start();
+		final Base base = new PgBase(
+			source,
+			new AppInfoImpl(Membership.NAME, args),
+			wss
+		);
         new FtCli(
 			new TkApp(
 				new FkRegex(
