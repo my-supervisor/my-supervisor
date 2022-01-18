129dbe99aeb0c436f7f4dc843fd1aa83f31fc75f
Olivier B. OURA <baudoliver7@gmail.com>
2022-01-18 23:50:41 +0000

    test: reproduce bug Ref: #9

--- src/main/java/com/minlessika/sdk/takes/XeMIdentity.java
+++ src/main/java/com/minlessika/sdk/takes/XeMIdentity.java
@@ -29,66 +29,66 @@ public final class XeMIdentity extends XeWrap {
 		
 		final Identity identity = new RqAuth(req).identity();
 		
-		final Long id;
+		final XeSource result;
 		if(identity == Identity.ANONYMOUS) {
-			id = 2L;
+			final Long id = 2L;
+			result = XeSource.EMPTY;
 		} else {
-			id = Long.parseLong(identity.properties().get("id"));
-		}
-		
-    	final List<ResultStatement> results = base.query(
-							    			  "select "
-							    			+ "pers.photo, pers.name, addr.email, pers.is_company, pers.photo "
-							    			+ "from base_person as pers "
-							    			+ "left join base_address as addr on addr.id = pers.address_id "
-							    			+ "where pers.id=?",
-							    			Arrays.asList(id)
-							    		);
-    	final Map<String, Object> data = results.get(0).data();
-		
-    	final String module = base.appInfo().currentModule();
-		final List<ResultStatement> resultsProfile = base.query(
-  			  "select "
-  			+ "prof.id as profile_id, prof.name as profile, prof.tag as profile_tag "
-  			+ "from base_application as app "
-  			+ "left join membership_profile as prof on prof.id = app.profile_id "
-  			+ "where app.user_id=? and app.module = ?",
-  			Arrays.asList(id, module)
-  		);
-		
-		final String PROFILE_TAG = "profile_tag";
-		
-		final Map<String, Object> dataProfile;
-		if(resultsProfile.isEmpty()) {
-			dataProfile = new LinkedHashMap<>();
-			dataProfile.put("profile_id", 0);
-			dataProfile.put("profile", StringUtils.EMPTY);
-			dataProfile.put(PROFILE_TAG, StringUtils.EMPTY);			
-		} else {
-			dataProfile = resultsProfile.get(0).data();   
-		}
+			final Long id = Long.parseLong(identity.properties().get("id"));
+			final List<ResultStatement> results = base.query(
+					"select "
+							+ "pers.photo, pers.name, addr.email, pers.is_company, pers.photo "
+							+ "from base_person as pers "
+							+ "left join base_address as addr on addr.id = pers.address_id "
+							+ "where pers.id=?",
+					Arrays.asList(id)
+			);
+			final Map<String, Object> data = results.get(0).data();
 
-    	return new XeSource() {
-			
-			@Override
-			public Iterable<Directive> toXembly() throws IOException {
-				return new Directives()
-		                .add("identity")
-		                .add("id").set(id).up()
-		                .add("name").set(data.get("name")).up()
-		                .add("email").set(data.get("email")).up()
-		                .add("profile").set(dataProfile.get("profile")).up()
-		                .add("profile_id").set(dataProfile.get("profile_id")).up()
-		                .add(PROFILE_TAG).set(dataProfile.get(PROFILE_TAG)).up()
-		                .add("is_admin").set(String.format("ADMIN_%s", module.toUpperCase()).equals(dataProfile.get(PROFILE_TAG))).up()
-		                .add("is_anonymous").set(String.format("ANONYMOUS_%s", module.toUpperCase()).equals(dataProfile.get(PROFILE_TAG))).up()
-		                .add("is_simple_user").set(String.format("USER_%s", module.toUpperCase()).equals(dataProfile.get(PROFILE_TAG))).up()
-		                .add("is_company").set(data.get("is_company")).up()
-		                .add("photo").set(data.get("photo")).up()
-		                .up();
+			final String module = base.appInfo().currentModule();
+			final List<ResultStatement> resultsProfile = base.query(
+					"select "
+							+ "prof.id as profile_id, prof.name as profile, prof.tag as profile_tag "
+							+ "from base_application as app "
+							+ "left join membership_profile as prof on prof.id = app.profile_id "
+							+ "where app.user_id=? and app.module = ?",
+					Arrays.asList(id, module)
+			);
+
+			final String PROFILE_TAG = "profile_tag";
+
+			final Map<String, Object> dataProfile;
+			if(resultsProfile.isEmpty()) {
+				dataProfile = new LinkedHashMap<>();
+				dataProfile.put("profile_id", 0);
+				dataProfile.put("profile", StringUtils.EMPTY);
+				dataProfile.put(PROFILE_TAG, StringUtils.EMPTY);
+			} else {
+				dataProfile = resultsProfile.get(0).data();
 			}
-		};
-		
+
+			result = new XeSource() {
+
+				@Override
+				public Iterable<Directive> toXembly() throws IOException {
+					return new Directives()
+							.add("identity")
+							.add("id").set(id).up()
+							.add("name").set(data.get("name")).up()
+							.add("email").set(data.get("email")).up()
+							.add("profile").set(dataProfile.get("profile")).up()
+							.add("profile_id").set(dataProfile.get("profile_id")).up()
+							.add(PROFILE_TAG).set(dataProfile.get(PROFILE_TAG)).up()
+							.add("is_admin").set(String.format("ADMIN_%s", module.toUpperCase()).equals(dataProfile.get(PROFILE_TAG))).up()
+							.add("is_anonymous").set(String.format("ANONYMOUS_%s", module.toUpperCase()).equals(dataProfile.get(PROFILE_TAG))).up()
+							.add("is_simple_user").set(String.format("USER_%s", module.toUpperCase()).equals(dataProfile.get(PROFILE_TAG))).up()
+							.add("is_company").set(data.get("is_company")).up()
+							.add("photo").set(data.get("photo")).up()
+							.up();
+				}
+			};
+		}
+		return result;
 	}
 
 }
---
+++ src/test/java/com/minlessika/membership/takes/TkIndexTest.java
@@ -0,0 +1,31 @@
+package com.minlessika.membership.takes;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.takes.rq.RqFake;
+import org.takes.rs.RsPrint;
+
+/**
+ * Test case for {@link TkIndex}
+ */
+final class TkIndexTest {
+
+    /** @todo #9:30min fix TkIndex and enable this test.
+    *    This test fails because we aren't able to locate xsl file
+     *   {@code /com/membership/xsl/index/page_en_US.xsl}. To fix it,
+     *   we wrote firstly this test to reproduce the bug. Now we should
+     *   fix it and enable this test.
+    **/
+    @Test
+    @Disabled
+    void returnsHttpResponse() throws Exception {
+        MatcherAssert.assertThat(
+            new RsPrint(
+                new TkIndex(new FakeBase()).act(new RqFake("GET", "/"))
+            ).printBody(),
+            new StringContains("Home")
+        );
+    }
+}
