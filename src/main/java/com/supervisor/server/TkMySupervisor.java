3bbec072b3c7574234bc572c28ae0a880118bc01
Olivier B. OURA <baudoliver7@gmail.com>
2022-02-01 03:55:12 +0000

    docs: remove puzzle Ref: #41

--- src/main/java/com/supervisor/server/TkMySupervisor.java
+++ src/main/java/com/supervisor/server/TkMySupervisor.java
@@ -1,29 +1,13 @@
 package com.supervisor.server;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
 import com.supervisor.sdk.datasource.Base;
 import com.supervisor.sdk.datasource.SqlTxn;
 import com.supervisor.sdk.takes.TkBaseWrap;
 import com.supervisor.sdk.translation.I18n;
 import org.takes.Take;
-
-import com.supervisor.billing.PlannedTask;
-import com.supervisor.billing.PlannedTasks;
-import com.supervisor.billing.PurchaseOrder;
-import com.supervisor.domain.PlanSubscriptionContract;
-import com.supervisor.domain.Profile;
 import com.supervisor.domain.User;
 import com.supervisor.takes.RqUser;
-import com.supervisor.domain.Supervisor;
-import com.supervisor.domain.impl.PxSupervisor;
 
-/**
- * @todo #32:30min Fix internal error while requesting login page.
- *  We see an internal error when we want to show login page.
- *  Fix it and enable login and logout.
- */
 public final class TkMySupervisor extends TkBaseWrap {
 
 	public TkMySupervisor(final Base base, final Take take) {
