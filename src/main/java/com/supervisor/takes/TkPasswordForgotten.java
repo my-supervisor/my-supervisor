4324a2173e6ff943122eab4b3a5b613108fbbe83
Olivier B. OURA <baudoliver7@gmail.com>
2022-02-11 03:45:18 +0000

    feat(recaptcha): protect forgotten password page
    We protect the form and all actions related to it.
    
    Ref: #60

--- src/main/java/com/supervisor/takes/TkPasswordForgotten.java
+++ src/main/java/com/supervisor/takes/TkPasswordForgotten.java
@@ -1,20 +1,22 @@
 package com.supervisor.takes;
 
 import com.supervisor.sdk.datasource.Base;
+import com.supervisor.sdk.secure.Recaptcha;
 import com.supervisor.sdk.takes.TkForm;
 import com.supervisor.sdk.takes.XeRecaptcha;
 import com.supervisor.sdk.utils.OptUUID;
-import org.takes.Request;
-import org.takes.rs.xe.XeSource;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.takes.Request;
+import org.takes.rs.xe.XeSource;
 
 public final class TkPasswordForgotten extends TkForm {
 
-	public TkPasswordForgotten(final Base base) {
+	private final Recaptcha recaptcha;
+	public TkPasswordForgotten(final Base base, final Recaptcha recaptcha) {
 		super(base);
+		this.recaptcha = recaptcha;
 	}
 
 	@Override
@@ -26,7 +28,7 @@ public final class TkPasswordForgotten extends TkForm {
 	protected Iterable<XeSource> contentToShow(Request req, XeSource itemToShow) throws IOException {
 		List<XeSource> content = new ArrayList<>();
 		content.add(itemToShow);
-		// content.add(new XeRecaptcha(base.appInfo()));
+		content.add(new XeRecaptcha(recaptcha));
 		
 		return content;
 	}
