d3b7f2214b8dbbf12bd5458c4322786916b79f91
Olivier B. OURA <baudoliver7@gmail.com>
2022-02-21 01:48:39 +0000

    chore: setup dockerfile
    Resolve: #94

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+java --add-opens java.base/java.lang=ALL-UNNAMED \
+  -cp /usr/lib/my-supervisor/my-supervisor.jar:/usr/lib/my-supervisor/lib/* \
+  com.supervisor.server.Main --port=8080 --ws-port=8081 \
+  --database=${DB_NAME} --db-host=${DB_HOST} --db-port=${DB_PORT} \
+  --db-user=${DB_USER} --db-password=${DB_PASSWORD}
